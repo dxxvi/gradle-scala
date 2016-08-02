472bbf1c9af6024f4b446fc64d14f86b0000b531
Thai <dxxvi@yahoo.com>
2016-08-02 07:14:26 -0400

    experiment with Java 8's Optional (will implement the same with Scala to see the difference).

--- scala/src/test/java/home/Optional/OptionalExperiment.java
+++ scala/src/test/java/home/Optional/OptionalExperiment.java
@@ -2,6 +2,16 @@ package home.Optional;
 
 import org.junit.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 /**
  * Created by ly on 7/31/16.
  */
@@ -12,6 +22,27 @@ public class OptionalExperiment {
      * Check the Scala version of the implementation as well.
      */
     @Test public void f() {
+        Function<Path, Stream<String>> getLines = p -> {
+            try {
+                return Files.lines(p);
+            }
+            catch (IOException ioex) {
+                return Stream.empty();
+            }
+        };
+        Function<String, Optional<Integer>> convertToInteger = s -> {
+            try {
+                return Optional.of(Integer.valueOf(s));
+            }
+            catch (Exception ex) {
+                return Optional.empty();
+            }
+        };
+        Stream<Integer> stream = getLines.apply(Paths.get("/dev/shm/test.txt"))
+                .map(convertToInteger)
+                .filter(Optional::isPresent)
+                .map(Optional::get);
 
+        System.out.println(Arrays.toString(stream.mapToInt(i -> i).toArray()));
     }
 }
